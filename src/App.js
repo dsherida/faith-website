523a9c97b7f57d92709fe5ec59f166353c338726
Doug Sheridan <dsherida@asu.edu>
2018-01-01 19:44:17 -0700

    Configure for github pages and add dummy header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Component imports
+import Head from './components/head';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Head />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/components/head.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Head extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <p>This is the header</p>
+            </div>
+        );
+    }
+}
