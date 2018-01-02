52c4e92d201bf1aba7212b5b89eef0c37799eed9
Doug Sheridan <dsherida@asu.edu>
2018-01-01 20:33:49 -0700

    Fix Flow config

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 // Component imports
 import Head from './Head';
